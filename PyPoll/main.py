047a4edd3dc777e6d12b5185da78b57f9c76c678
Kirstie McCown <accounts@intimemechanical.com.au>
2020-06-26 23:11:57 +0800

    Edited PyPoll set coding

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,11 +7,14 @@ import csv
 # Set file path
 election_csv = os.path.join("Resources", "election_data.csv")
 
-# Set a variable for the net total amount of votes in the dataset (exclude header row)
+# Set a variable for the total amount of votes in the dataset (exclude header row)
 total_votes = 0
 
-# Create empty lists to hold the list of candidates
+candidate_votes = 0
+
+# Create an empty list to hold the list of candidates
 candidates = []
+
 
 # Open and read csv file 
 with open(election_csv) as csvfile:
@@ -27,27 +30,20 @@ with open(election_csv) as csvfile:
         total_votes = int(total_votes+1)
 
 # Print to check total votes is being calculated correctly
-print(total_votes)
+# print(total_votes)
 
-        # Append the candidates into the list
+# Append the candidates into the list
         candidates.append(row[2])
-          
-        # Create a set with only unique candidate values 
-        unique_candidates = set(candidates)
 
-print(unique_candidates)
+# Create a set with only unique candidate values 
+unique_candidates = set(candidates)
+# print(len(unique_candidates))
+# print(unique_candidates)
         
-        # profit_increase.append(float(row[1]))
-        # profit_increase.sort()
-        # profit_increase = profit_increase[-1]
-        # print(profit_increase)
-
-        # profit_i_month.append(str(row[0]))
-        # print(max(profit_i_month))
-
-        # Find greatest decrease in profits (date and amount) over the entire period
-        # profit_decrease
-        # profit_d_month
+# Calculate total votes per candidate 
+if (row[2]) == "Khan":
+    candidate_votes = candidate_votes +1 
+print(candidate_votes)
 
 # print the analysis to the terminal and export a text file with the results
 
@@ -59,11 +55,14 @@ print("----------------------------")
 print(f"Total Votes: {total_votes}")
 print("----------------------------")
 
-# # # Print net total profit/loss  
-# # print(f"Total: ${total_p_l:.0f}")
+# Print total number and percentage of votes per candidate
+for names in unique_candidates:
+    print(names) 
+# 
+#
+#
+# print("----------------------------")
 
-# # # Print average change in profit/loss
-# # print(f"Average Change: ${average_change:.2f}")
-
-# # # print(f"Greatest Increase in Profits: {profit_i_month} {profit_increase:.2f}")
-# # # # print(f"Greatest Decrease in Profits: {profit_d_month} "({profit_decrease:.2f})""
+# Print the winner of the election
+# print(f"Winner: {}")
+# print("----------------------------")
